1f157a5b90356112b343a6416d5e14de8dfa4eb5
34678 <3383789730@qq.com>
2018-02-09 00:59:31 +0800

    抽象组件完成

---
+++ JDFinance-setup/app/core/btn.vue
@@ -0,0 +1,29 @@
+<template lang="html">
+    <div :class="[btnClass,cname]">
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+  // 允许自定义类 可以自定义文本
+  export default {
+    props: {
+      cname: {
+        type: String,
+        default: ''
+      }
+    },
+    data() {
+      return {
+        btnClass: 'btn'
+      }
+    }
+  }
+</script>
+<style lang="css">
+    /*这里使用sass的btn样式*/
+    @import "../css/element.scss";
+    .btn {
+      @include btn
+    }
+</style>
