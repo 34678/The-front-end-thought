beb3c3a4ccab1b86d29752796acc31f04efc3c4b
34678 <3383789730@qq.com>
2018-02-09 15:06:10 +0800

    抽象组件完成
    补充了一个silder

--- JDFinance-setup/app/core/slider.vue
+++ JDFinance-setup/app/core/slider.vue
@@ -1,12 +1,53 @@
 <template lang="html">
     <div>
-
+        <section :class="cname">
+            <swiper :options="options" :no-next-tick="options.noNextTick">
+                <swiper-slide v-for="item in items" :key="item.href">
+                    <router-link :to="{name:item.href}">
+                        <img :src="item.src" alt="">
+                    </router-link>
+                </swiper-slide>
+                <div class="swiper-pagination" vif="options.pagination"></div>
+            </swiper>
+        </section>
     </div>
 </template>
 
 <script>
-  export default {}
+  import {swiper, swiperSlide} from "vue-awesome-swiper"
+
+  export default {
+    components: {
+      swiperSlide,
+      swiper
+    },
+    props: {
+      cname: {
+        type: String,
+        default: ''
+      },
+      options: {
+        type: Object,
+        default() {
+          return {
+            autoplay: true,
+            loop: true,
+            pegination: {
+              el: '.swiper-pagination',
+            },
+            notNextTick: false
+          }
+        }
+      },
+      item: {
+        type: Array,
+        default() {
+          return []
+        }
+      }
+    }
+  }
 </script>
 <style lang="css">
-
+    @import "~swiper/dist/css/swiper.css";
 </style>
