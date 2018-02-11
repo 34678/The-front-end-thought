9cf0ca282934a1afd9bd89a698b53d525bdf75b3
34678 <3383789730@qq.com>
2018-02-11 20:49:48 +0800

    理财组件

---
+++ JDFinance-setup/app/js/money/index.vue
@@ -0,0 +1,28 @@
+<template lang="html">
+    <div>
+        <heador></heador>
+
+        <footor></footor>
+        <navbar></navbar>
+    </div>
+</template>
+
+<script>
+  import Heador from '../public/header'
+  import Navbar from '../public/navbar'
+  import Footor from '../public/footer'
+  import MSlider from '../money/mslider'
+
+  export default {
+    components: {
+      Navbar,
+      Heador,
+      MSlider,
+      Footor
+    }
+  }
+</script>
+
+<style lang="scss" module>
+    @import '../../css/reset.scss';
+</style>
