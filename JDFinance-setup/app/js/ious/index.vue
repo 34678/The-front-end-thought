71208c091eaffb5fba69267cd4c2f6f23bc2a035
34678 <3383789730@qq.com>
2018-02-11 22:06:03 +0800

    白条模块
    一点还没做完

---
+++ JDFinance-setup/app/js/ious/index.vue
@@ -0,0 +1,35 @@
+<template lang="html">
+    <div>
+        <heador></heador>
+<islider></islider>
+        <welfare>
+
+        </welfare>
+        <service></service>
+        <footor></footor>
+        <navbar></navbar>
+    </div>
+</template>
+
+<script>
+  import Heador from '../public/header'
+  import Navbar from '../public/navbar'
+  import Footor from '../public/footer'
+  import Islider from '../ious/islider'
+  import Welfare from '../ious/welfare'
+  import Service from '../ious/service'
+
+  export default {
+    components: {
+      Navbar,
+      Heador,
+      Footor,
+      Islider,
+      Welfare,
+      Service
+
+    }
+  }
+</script>
+
+
